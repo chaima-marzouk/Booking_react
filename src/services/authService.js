31bd3c4315831702d85a9f50f5918be0035a7cf2
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-02-17 12:23:58 +0100

    new changes

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,18 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-const apiURL = process.env
+const apiURL = process.env.REACT_APP_API_URL;
+
+export function login(data){
+    return (
+        axios.post(`${apiURL}/signup`, data)
+    )
+}
+
+export function getCurrentUser(){
+    try{
+        const token = localStorage.getItem("token");
+        return jwtDecode(token)
+    }catch(error){
+        return null;
+    }
+}
