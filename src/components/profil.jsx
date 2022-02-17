c590f3334b1414d6e92ca3f85d5dc1e0cfdc8752
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-02-17 05:43:11 +0100

    🥚 create a route for profil

---
+++ src/components/profil.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Profil(){
+    return (
+        <div>
+            <h1>Welcome to profil</h1>
+        </div>
+    )
+}
+export default Profil;
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import reactDom from 'react-dom';
 import Signin_form from './components/signin_form';
 import Signup_form from './components/signup_form';
+import Profil from './components/profil';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
         <Route path="/signup" element={<Signup_form />} />
         <Route path="/signin" element={<Signin_form />} />
+        <Route path="/profil/:id" element={<Profil />} />
     </Routes>
     </div>
     );
