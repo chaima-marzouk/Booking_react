00e89cb83f81930ac921547aa02686fb2208fa67
AMINE.ZOUHAID <amine2020zouhaid@gmail.com>
2022-02-23 14:24:38 +0100

    modification hotel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {Routes, Route} from "react-router-dom"
 import SignIn from './components/authentification/signIn';
 import SignUp from './components/authentification/signUp';
 import Profil from './components/profil';
+import Hotel from './components/hotel/HotelList';
+
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,8 @@ function App() {
         <Route path='signin' element= {<SignIn/>}/>
         <Route path='signup' element= {<SignUp/>}/>
         <Route path='profil' element= {<Profil/>}/>
+        <Route path='HotelList' element= {<Hotel/>}/>
+
       </Routes>
 
     </div>
---
+++ src/components/hotel/HotelList.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import React , {Component} from "react";
+
+
+
+
+
+export default class HotelList extends  Component {
+
+state ={
+    myArray: []
+};
+    componentDidMount(){
+        axios.get("");
+    }
+
+
+    render(){
+        return (
+<div></div>
+        )
+    }
+}
