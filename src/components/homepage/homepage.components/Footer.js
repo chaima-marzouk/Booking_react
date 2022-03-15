85d119ce963a5031cecba17df0e748339122fb5b
aces96 <esraidi12@gmail.com>
2022-03-15 14:56:25 +0100

    i almost finished home page

--- src/components/homepage/homepage.components/Footer.js
+++ src/components/homepage/homepage.components/Footer.js
@@ -1,27 +1,48 @@
-import { Paper } from "@mui/material";
+import { Paper, TextField } from "@mui/material";
 import { Grid } from "@mui/material";
-
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 
 const Footer = (props)=>{
 
 
     return (
-            <Paper sx={{height:'20vh', width: "100%", backgroundColor: "#141E27"}}>
-                <Grid container spacing={1}>
-                    <Grid item xs={4}>
-                        
+            <Paper sx={{height:'40vh', width: "100%",backgroundColor: "#0F0E0E", border: "1px white solid"}}>
+                <Grid sx={{height: "100%"}} container spacing={0}>
+                    <Grid item xs={4} container direction="column"   alignItems="center" justifyContent="center">
+                        <Typography sx={{color:'white'}}  variant="h3">
+                            LOGO
+                        </Typography>
                     </Grid>
 
-                    <Grid item xs={4}>
+
+
+                    <Grid item xs={4} container direction="column"   alignItems="center" justifyContent="center">
+                        <Typography variant="body1" sx={{color:'white',marginBottom:'10px'}}>
+                            Contact Us:
+                        </Typography>
+                        <TextField  id="outlined-basic" label="Email" variant="filled" color="warning" sx={{backgroundColor: "rgba(238,238,238,0.2)", marginBottom:'10px',width: "50%"}}/>
+                        <TextField id="outlined-basic" label="Your Message" variant="filled" color="warning" sx={{backgroundColor: "rgba(238,238,238,0.2)", marginBottom:'10px',width: "50%"}} />
+                        <Button sx={{width: "40%", marginTop: "5px"}} variant="contained" color="warning">Submit</Button>
 
                     </Grid>
 
-                    <Grid item xs={4}>
+                    <Grid sx={{height: "100%"}} container direction="column"   alignItems="center" justifyContent="center" item xs={4}>
+                        <Typography sx={{color:'white',marginBottom: "10px"}}  variant='body1'>
+                            Contact Us: 0645168595
+                        </Typography>
 
+                        <Typography sx={{color:'white',marginBottom: "10px"}}  variant='body1'>
+                            address: Qu ElQods N353 Youssoufia
+                        </Typography>
+
+                        <Typography sx={{color:'white', marginBottom: "10px"}}  variant='body1'>
+                            Email: esraidi12@gmail.com
+                        </Typography>
                     </Grid>
-
                 </Grid>
+
 
             </Paper>
     )
--- src/components/homepage/homepage.components/section.stat.js
+++ src/components/homepage/homepage.components/section.stat.js
@@ -4,6 +4,9 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Paper } from "@mui/material";
+import HotelIcon from '@mui/icons-material/Hotel';
+import RoomIcon from '@mui/icons-material/Room';
+import PersonIcon from '@mui/icons-material/Person';
 import '@fontsource/roboto/700.css';
 
 
@@ -11,23 +14,32 @@ import '@fontsource/roboto/700.css';
 const StatSection = (props)=>{
     const [hotels, sethotel] = useState(0)
     const [rooms, setroom] = useState(0)
+    const [users, setuser] = useState(0)
 
 
 
     useEffect(async ()=>{
-        const hotel = await axios.get('http://localhost:8080/api/hotels')
+        const hotel =  axios.get('http://localhost:8080/api/hotels')
                                 .then((res)=>{
                                     sethotel(res.data.length)
                                     console.log(res.data.length)
                                 })
                                 .catch((error) => console.log(error))
 
-        const room = await axios.get('http://localhost:8080/api/rooms')
+        const room =  axios.get('http://localhost:8080/api/rooms')
                                 .then((res)=>{
                                     setroom(res.data.length)
                                     console.log(res.data.length)
                                 })
                                 .catch((error)=> console.log(error))
+        
+        const user =  axios.get('http://localhost:8080/api/users')
+                                .then((res)=>{
+                                    setuser(res.data.users.length)
+                                    console.log(res.data.users.length)
+                                })
+                                .catch((error)=> console.log(error))
+
         
     })
 
@@ -37,26 +49,57 @@ const StatSection = (props)=>{
         <Box sx={{height: '40vh', width: "100%", backgroundColor: "#0F0E0E", margin: "auto"}}>
             <Paper sx={{width: "70%", height: "100%",backgroundColor: 'rgba(238,238,238,0)', margin: "auto"}}>
                 <Grid sx={{height: "100%", padding: 0, margin: 0}} container spacing={4}>
-                    <Grid  sx={{padding: '0px', margin: 0}} item  xs={6}>
-                        <Typography sx={{color: "white"}} variant="h4">Hotels Number</Typography>
-                        <Paper sx={{display: "flex", justifyContent:"center", alignItems:"center",width: "50%", height: "50%",margin: "auto", marginTop: "15px",backgroundColor: 'rgba(238,238,238,0)', border: "1px white solid",'&:hover':{border: "2px #ed6c02 solid"}}}>
-                            <Typography sx={{color:"white"}}  variant="h2">
+                    <Grid  sx={{padding: '0px', margin: 0}} item  xs={4}>
+                        <Paper sx={{display: "flex", justifyContent:"center", alignItems:"center",width: "50%", height: "50%",margin: "auto", marginTop: "15px",backgroundColor: 'rgba(238,238,238,0)','&:hover':{border: "2px #ed6c02 solid"}}}>
+                            <Typography sx={{color:"#ed6c02"}}  variant="h2">
                                 {hotels}
                             </Typography>
 
                         </Paper>
+                        <Grid container spacing={0} >
+                            <Grid  item xs={12} container justifyContent="center">
+                                <HotelIcon sx={{ fontSize: 30 }} color="warning" />
+                                <Typography sx={{color: "white",marginLeft: "10px"}} variant="h5">Hotels Number</Typography>
+                            </Grid>
+
+
+                        </Grid>
 
                         
 
                     </Grid>
 
-                    <Grid item  xs={6}>
-                    <Typography sx={{color: "white"}}  variant="h4">Rooms Number</Typography>
-                        <Paper sx={{display: "flex", justifyContent:"center",alignItems:"center",width: "50%", height: "50%",  margin: "auto",marginTop: "15px",backgroundColor: 'rgba(238,238,238,0)', border: "1px white solid",'&:hover':{border: "2px #ed6c02 solid"}}}>
-                        <Typography sx={{color:"white"}} variant="h2">
+                    <Grid item  xs={4}>
+                        <Paper sx={{display: "flex", justifyContent:"center",alignItems:"center",width: "50%", height: "50%",  margin: "auto",marginTop: "15px",backgroundColor: 'rgba(238,238,238,0)','&:hover':{border: "2px #ed6c02 solid"}}}>
+                        <Typography sx={{color:"#ed6c02"}} variant="h2">
                                 {rooms}
-                            </Typography>
+                        </Typography>
                         </Paper>
+                        <Grid container spacing={0} >
+                            <Grid  item xs={12} container justifyContent="center">
+                                <RoomIcon sx={{ fontSize: 30 }} color="warning" />
+                                <Typography sx={{color: "white", marginLeft: "10px"}}   variant="h5">Rooms Number</Typography>
+                            </Grid>
+
+
+                        </Grid>
+                    </Grid>
+
+
+                    <Grid item  xs={4}>
+                    <Paper sx={{display: "flex", justifyContent:"center",alignItems:"center",width: "50%", height: "50%",  margin: "auto",marginTop: "15px",backgroundColor: 'rgba(238,238,238,0)','&:hover':{border: "2px #ed6c02 solid"}}}>
+                        <Typography sx={{color:"#ed6c02"}} variant="h2">
+                                {users}
+                        </Typography>
+                        </Paper>
+                        <Grid container spacing={0} >
+                            <Grid  item xs={12} container justifyContent="center">
+                                <PersonIcon sx={{ fontSize: 30 }} color="warning" />
+                                <Typography sx={{color: "white", marginLeft: "10px"}}   variant="h5">Client Number</Typography>
+                            </Grid>
+
+
+                        </Grid>
 
                     </Grid>
 
